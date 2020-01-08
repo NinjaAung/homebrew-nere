5a4288e0a9c52b92005bacc48bc89ce5d2db083e
ninjaaung <ninja.aung02@gmail.com>
2020-01-08 05:49:42 -0800

    _

--- nere.rb
+++ nere.rb
@@ -1,7 +1,7 @@
 class Nere < Formula
   desc "A command line tool to create, delete and update repositories"
-  homepage "https://github.com/TheHipbot/weather"
-  url "https://github.com/TheHipbot/weather/raw/master/Formula/nere-1.0.0.tar.gz"
+  homepage "https://github.com/ninjaaung/nere"
+  url "https://github.com/ninjaaung/nere/raw/master/Formula/nere-1.0.0.tar.gz"
   version "1.0.0"
 
   bottle :unneeded
