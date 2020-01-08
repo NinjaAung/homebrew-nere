1f05a2dffb77a739895752b4d389224914f0ed2a
ninjaaung <ninja.aung02@gmail.com>
2020-01-08 06:15:08 -0800

    Added SHA256

--- nere.rb
+++ nere.rb
@@ -3,6 +3,7 @@ class Nere < Formula
   homepage "https://github.com/ninjaaung/nere"
   url "https://github.com/ninjaaung/nere/raw/master/Formula/nere-1.0.0.tar.gz"
   version "1.0.0"
+  sha256 "fc99567ce80e707306a8784c57d6fc10d8c41590a438c1c2caad1f5ee7b3269a"
 
   bottle :unneeded
 
