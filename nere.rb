559b2bc5b169eda5462e9d6687f0485a89708b7e
ninjaaung <ninja.aung02@gmail.com>
2020-01-09 04:56:15 -0800

    Updated Version

--- nere.rb
+++ nere.rb
@@ -1,8 +1,8 @@
 class Nere < Formula
   desc "A command line tool to create, delete and update repositories"
   homepage "https://github.com/ninjaaung/nere"
-  url "https://github.com/ninjaaung/nere/raw/master/Formula/nere-1.0.0.tar.gz"
-  version "1.0.0"
+  url "https://github.com/ninjaaung/nere/raw/master/Formula/nere-1.0.2.tar.gz"
+  version "1.0.2"
   sha256 "fc99567ce80e707306a8784c57d6fc10d8c41590a438c1c2caad1f5ee7b3269a"
 
   bottle :unneeded
