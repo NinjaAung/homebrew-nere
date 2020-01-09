141d9f4c2e3fb079b9b716ee2cf167e79f8a15ea
Nyein Chan Aung <Ninja.aung02@gmail.com>
2020-01-09 12:03:13 -0800

    Removed sha256

--- nere.rb
+++ nere.rb
@@ -3,7 +3,6 @@ class Nere < Formula
   homepage "https://github.com/ninjaaung/nere"
   url "https://github.com/ninjaaung/nere/raw/master/Formula/nere-1.0.2.tar.gz"
   version "1.0.2"
-  sha256 "0b6a2260a08e6cab0d1b8a583b240da4953652e344b8cf4752df8c898860bc16"
 
   bottle :unneeded
 
